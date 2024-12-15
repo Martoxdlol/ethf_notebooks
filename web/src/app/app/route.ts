26e84e78c4b26bd0fa47733620bceae4195edfe1
Tomás Cichero <martoxdlol@gmail.com>
2024-12-15 15:13:51 -0300

    use rewrite for flutter app

--- web/src/app/app/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(req: Request) {
-    const html = await fetch(new URL('/app/index.html', req.url).toString()).then((r) => r.text())
-    return new Response(html, {
-        headers: {
-            'Content-Type': 'text/html',
-        },
-    })
-}
