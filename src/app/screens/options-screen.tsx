bc7fc4d115f78db8491e7cad790796f3b81a26bb
Tomás Cichero <martoxdlol@gmail.com>
2025-03-04 13:14:20 -0300

    feat: update options screen to use anchor tags for external links

--- src/app/screens/options-screen.tsx
+++ src/app/screens/options-screen.tsx
@@ -1,19 +1,17 @@
-import { Link } from 'react-router'
-
 export function OptionsScreen() {
-    const linkClassName = 'shrink-0 gap-4 flex border-b'
+    const linkClassName = 'shrink-0 gap-4 flex border-b px-4 py-2'
 
     // yyyy-mm-dd
     const today = new Date().toISOString().split('T')[0]
 
     return (
         <>
-            <Link to='/api/etiquetas.pdf' className={linkClassName}>
+            <a href='/api/etiquetas.pdf' className={linkClassName}>
                 Imprimir etiquetas
-            </Link>
-            <Link to={`/api/reservas/reservas-notebooks-${today}.pdf`} className={linkClassName}>
+            </a>
+            <a href={`/api/reservas/reservas-notebooks-${today}.pdf`} className={linkClassName}>
                 Imprimir reservas de hoy
-            </Link>
+            </a>
             <a href='https://inventario.henryford.edu.ar' className={linkClassName}>
                 Inventario ETHF
             </a>
